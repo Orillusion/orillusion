7e0231ce14a0f10b85611dff4913082b468fdd09
sirxu <397939361@qq.com>
2023-04-23 17:40:40 +0800

    feat(Light-BindGroup):
    add global light bindGroup at scene3D

---
+++ src/engine/gfx/generate/graphic/webgpu/core/bindGroups/groups/LightEntries.ts
@@ -0,0 +1,68 @@
+import { LightData } from '../../../../../../components/lights/LightData';
+import { MemoryInfo } from '../../../../../../core/pool/memory/MemoryInfo';
+import { View3D } from '../../../../../../core/View3D';
+import { Engine3D } from '../../../../../../Engine3D';
+import { EntityCollect } from '../../../../../renderJob/collect/EntityCollect';
+import { DDGIIrradianceVolume } from '../../../../../renderJob/passRenderer/ddgi/DDGIIrradianceVolume';
+import { StorageGPUBuffer } from '../../buffer/StorageGPUBuffer';
+
+//TODO dynamic lights need fixed
+/**
+ * @internal
+ * @group GFX
+ */
+export class LightEntries {
+    public storageGPUBuffer: StorageGPUBuffer;
+    public irradianceVolume: DDGIIrradianceVolume;
+    private _lightList: MemoryInfo[] = [];
+
+    constructor() {
+        this.storageGPUBuffer = new StorageGPUBuffer(
+            LightData.lightSize * Engine3D.setting.light.maxLight,
+            GPUBufferUsage.COPY_SRC
+        );
+
+        for (let i = 0; i < Engine3D.setting.light.maxLight; i++) {
+            let memory = this.storageGPUBuffer.memory.allocation_node(LightData.lightSize * 4);
+            this._lightList.push(memory);
+        }
+
+        this.storageGPUBuffer.visibility = GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT | GPUShaderStage.COMPUTE;
+        this.irradianceVolume = new DDGIIrradianceVolume();
+        this.irradianceVolume.init(Engine3D.setting.gi);
+    }
+
+    public update(view: View3D) {
+        let lights = EntityCollect.instance.getLights(view.scene);
+        for (let i = 0; i < lights.length; i++) {
+            const light = lights[i].lightData;
+            this.writeLightBytes(light, this._lightList[i]);
+        }
+        this.storageGPUBuffer.apply();
+    }
+
+    private writeLightBytes(light: LightData, memory: MemoryInfo) {
+        memory.offset = 0;
+        memory.writeFloat(light.index);
+        memory.writeInt32(light.lightType);
+        memory.writeFloat(light.radius);
+        memory.writeFloat(light.linear);
+
+        memory.writeVector3(light.lightPosition);
+        memory.writeFloat(light.lightMatrixIndex);
+
+        memory.writeVector3(light.direction);
+        memory.writeFloat(light.quadratic);
+
+        memory.writeRGBColor(light.lightColor);
+        memory.writeFloat(light.intensity);
+
+        memory.writeFloat(light.innerAngle);
+        memory.writeFloat(light.outerAngle);
+        memory.writeFloat(light.range);
+        memory.writeInt32(light.castShadowIndex);
+
+        memory.writeVector3(light.lightTangent);
+        memory.writeFloat(-1);
+    }
+}
