af00b02c3276a83d40c434b4e39deca40e9541f7
sirxu <397939361@qq.com>
2023-04-24 15:25:15 +0800

    feat(render): add render framework
    add engine render frame work

---
+++ src/engine/gfx/renderJob/collect/ShadowLightsCollect.ts
@@ -0,0 +1,165 @@
+import { CameraUtil, Engine3D, UUID } from '../../../..';
+import { DirectLight } from '../../../components/lights/DirectLight';
+import { LightBase } from '../../../components/lights/LightBase';
+import { LightType } from '../../../components/lights/LightData';
+import { Scene3D } from '../../../core/Scene3D';
+import { View3D } from '../../../core/View3D';
+/**
+ * @internal
+ * @group Lights
+ */
+export class ShadowLightsCollect {
+
+    public static directionLightList: Map<Scene3D, DirectLight[]>;
+    public static pointLightList: Map<Scene3D, LightBase[]>;
+
+    public static init() {
+        this.directionLightList = new Map<Scene3D, DirectLight[]>();
+        this.pointLightList = new Map<Scene3D, LightBase[]>();
+    }
+
+    static getShadowLightList(light: LightBase) {
+        if (!light.transform.view3D) return null;
+        if (light.lightData.lightType == LightType.DirectionLight) {
+            let list = this.directionLightList.get(light.transform.view3D.scene);
+            if (!list) {
+                list = [];
+                this.directionLightList.set(light.transform.view3D.scene, list);
+            }
+            return list;
+        } else if (light.lightData.lightType == LightType.PointLight) {
+            let list = this.pointLightList.get(light.transform.view3D.scene);
+            if (!list) {
+                list = [];
+                this.pointLightList.set(light.transform.view3D.scene, list);
+            }
+            return list;
+        } else if (light.lightData.lightType == LightType.SpotLight) {
+            let list = this.pointLightList.get(light.transform.view3D.scene);
+            if (!list) {
+                list = [];
+                this.pointLightList.set(light.transform.view3D.scene, list);
+            }
+            return list;
+        }
+    }
+
+    static getShadowLightWhichScene(scene: Scene3D, type: LightType) {
+        if (type == LightType.DirectionLight) {
+            let list = this.directionLightList.get(scene);
+            if (!list) {
+                list = [];
+                this.directionLightList.set(scene, list);
+            }
+            return list;
+        } else if (type == LightType.PointLight) {
+            let list = this.pointLightList.get(scene);
+            if (!list) {
+                list = [];
+                this.pointLightList.set(scene, list);
+            }
+            return list;
+        }
+    }
+
+    static getDirectShadowLightWhichScene(scene: Scene3D) {
+        let list = this.directionLightList.get(scene);
+        if (!list) {
+            list = [];
+            this.directionLightList.set(scene, list);
+        }
+        return list;
+    }
+
+    static getPointShadowLightWhichScene(scene: Scene3D) {
+        let list = this.pointLightList.get(scene);
+        if (!list) {
+            list = [];
+            this.pointLightList.set(scene, list);
+        }
+        return list;
+    }
+
+    static addShadowLight(light: LightBase) {
+        if (!light.transform.view3D) return null;
+        if (light.lightData.lightType == LightType.DirectionLight) {
+            let list = this.directionLightList.get(light.transform.view3D.scene);
+            if (!list) {
+                list = [];
+                this.directionLightList.set(light.transform.view3D.scene, list);
+            }
+            if (light instanceof DirectLight && !light.shadowCamera) {
+                light.shadowCamera = CameraUtil.createCamera3DObject(null, 'shadowCamera');
+                light.shadowCamera.name = UUID();
+                light.shadowCamera.isShadowCamera = true;
+                light.shadowCamera.orthoOffCenter(
+                    Engine3D.setting.shadow.shadowBound,
+                    -Engine3D.setting.shadow.shadowBound,
+                    Engine3D.setting.shadow.shadowBound,
+                    -Engine3D.setting.shadow.shadowBound,
+                    1,
+                    50000,
+                );
+            }
+            let has = list.indexOf(light as DirectLight) == -1;
+            if (has) {
+                if (list.length < 8) {
+                    light.lightData.castShadowIndex = list.length;
+                }
+                list.push(light as DirectLight);
+            }
+            return list;
+        } else if (light.lightData.lightType == LightType.PointLight) {
+            let list = this.pointLightList.get(light.transform.view3D.scene);
+            if (!list) {
+                list = [];
+                this.pointLightList.set(light.transform.view3D.scene, list);
+            }
+            let has = list.indexOf(light) == -1;
+            if (has) {
+                if (list.length < 8) {
+                    light.lightData.castShadowIndex = list.length;
+                }
+                list.push(light);
+            }
+            return list;
+        } else if (light.lightData.lightType == LightType.SpotLight) {
+            let list = this.pointLightList.get(light.transform.view3D.scene);
+            if (!list) {
+                list = [];
+                this.pointLightList.set(light.transform.view3D.scene, list);
+            }
+            let has = list.indexOf(light) == -1;
+            if (has) {
+                if (list.length < 8) {
+                    light.lightData.castShadowIndex = list.length;
+                }
+                list.push(light);
+            }
+            return list;
+        }
+    }
+
+    public static removeShadowLight(light: LightBase) {
+        if (!light.transform.view3D) return null;
+        if (light.lightData.lightType == LightType.DirectionLight) {
+            let list = this.directionLightList.get(light.transform.view3D.scene);
+            if (list) {
+                let index = list.indexOf(light as DirectLight);
+                if (index != -1) {
+                    list.splice(index, 1);
+                }
+            }
+            return list;
+        } else if (light.lightData.lightType == LightType.PointLight) {
+            let list = this.pointLightList.get(light.transform.view3D.scene);
+            if (list) {
+                let index = list.indexOf(light);
+                if (index != -1) {
+                    list.splice(index, 1);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ src/engine/gfx/renderJob/config/RenderLayer.ts
@@ -0,0 +1,30 @@
+/**
+ * 渲染类型标签
+ * @internal
+ * @group Post
+ */
+export enum RenderLayer {
+    None = 1 << 1,
+    StaticBatch = 1 << 2,
+    DynamicBatch = 1 << 3,
+}
+
+/**
+ * @internal
+ * @group GFX
+ */
+ export class RenderLayerUtil {
+    public static addMask(src: RenderLayer, tag: RenderLayer): RenderLayer {
+        let value = src | tag;
+        return value;
+    }
+
+    public static removeMask(src: RenderLayer, tag: RenderLayer): RenderLayer {
+        let value = src & (~tag);
+        return value;
+    }
+
+    public static hasMask(m1: RenderLayer, m2: RenderLayer): boolean {
+        return (m1 & m2) != 0;
+    }
+}
