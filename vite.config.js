4a256d9e2f219af236141d4c8910a1eee8120a2a
ShuangLiu <liu.shuang@huasheng.io>
2023-04-30 13:49:12 +0800

    fix: opt auto index on unlink files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import { readFile, writeFile, readdir, lstat } from 'fs/promises'
-import {resolve, parse} from 'path'
+import { resolve, parse } from 'path'
 
 module.exports = defineConfig({
     server: {
@@ -18,11 +18,39 @@ module.exports = defineConfig({
     plugins: [{
         name: 'autoIndex',
         configureServer(server) {
+            const tsFile = /\/src\/.*.ts$/
+            async function dir(folder, ts = []) {
+                let files = await readdir(folder)
+                for (let f of files) {
+                    let path = resolve(folder, f)
+                    let ls = await lstat(path)
+                    if (ls.isDirectory()) {
+                        await dir(path, ts)
+                    } else if (tsFile.test(path)) {
+                        let name = parse(path).name
+                        if (name !== 'index' && !name.startsWith('_') && !name.endsWith('-back'))
+                            ts.push(path)
+                    }
+                }
+                return ts
+            }
+            async function autoIndex(file) {
+                if(file && !tsFile.test(file))
+                    return
+                let ts = await dir('./src')
+                let improts = ''
+                for (let path of ts) {
+                    improts += `export * from "${path.replace(__dirname + '/src', '.').slice(0, -3)}"\r\n`
+                }
+                let content = await readFile(resolve(__dirname, './src/index.ts'), 'utf-8')
+                if (improts !== content) {
+                    console.log('[autoIndex] index.ts')
+                    writeFile(resolve(__dirname, './src/index.ts'), improts)
+                }
+            }
             server.httpServer.on('listening', autoIndex)
-            server.watcher.on('change', (file)=>{
-                if(file.match(/\/src\/.*.ts$/))
-                    autoIndex()
-            })
+            server.watcher.on('change', autoIndex)
+            server.watcher.on('unlink', autoIndex)
         }
     }],
     build: {
@@ -34,32 +62,3 @@ module.exports = defineConfig({
         // minify: 'terser'
     }
 })
-
-const tsFile = /\.ts$/
-async function dir(folder, ts=[]){
-    let files = await readdir(folder)
-    for(let f of files){
-        let path = resolve(folder, f)
-        let ls = await lstat(path)
-        if(ls.isDirectory()){
-            await dir(path, ts)
-        }else if(tsFile.test(path)){
-            let name = parse(path).name
-            if(name !== 'index' && !name.startsWith('_') && !name.endsWith('-back'))
-                ts.push(path)
-        }
-    }
-    return ts
-}
-async function autoIndex(file){
-    let ts = await dir('./src')
-    let improts = ''
-    for (let path of ts){
-        improts += `export * from "${path.replace(__dirname + '/src', '.').slice(0, -3)}"\r\n`
-    }
-    let content = await readFile(resolve(__dirname, './src/index.ts'), 'utf-8')
-    if(improts !== content){
-        console.log('[autoIndex] index.ts')
-        writeFile(resolve(__dirname, './src/index.ts'), improts)
-    }
-}
