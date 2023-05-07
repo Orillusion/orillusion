d2cd53cdaab9980897c1810e2eedf6224e8d88b6
ShuangLiu <liu.shuang@huasheng.io>
2023-05-07 08:06:35 +0800

    chore: bump packages version

--- packages/ammo/ammo.d.ts
+++ packages/ammo/ammo.d.ts
@@ -4,7 +4,7 @@ export default Ammo;
  */
 declare function Ammo<T>(target?: T): Promise<T & typeof Ammo>;
 /**
- * Ammo 原生类 by Bullet2
+ * Ammo.js by Bullet2
  */
 declare module Ammo {
     function destroy(obj: any): void;
