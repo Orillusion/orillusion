dd53a3de15f016a135ed876f589a69773a13fb11
hellmor <dymmky@126.com>
2023-05-10 14:25:15 +0800

    perf(sample): computeNormals
    ComputeNormals after update vertex position

--- samples/geometry/Sample_VertexAnimation.ts
+++ samples/geometry/Sample_VertexAnimation.ts
@@ -1,4 +1,5 @@
 import { View3D, PlaneGeometry, Engine3D, Scene3D, AtmosphericComponent, CameraUtil, HoverCameraController, Object3D, DirectLight, KelvinUtil, MeshRenderer, LitMaterial, VertexAttributeName, Time } from "@orillusion/core";
+import { GUIHelp } from "@orillusion/debug/GUIHelp";
 import { Stats } from "@orillusion/stats";
 import { GUIUtil } from "@samples/utils/GUIUtil";
 
@@ -8,6 +9,7 @@ class Smaple_VertexAnimation {
     floorGeometry: PlaneGeometry;
     scene: Scene3D;
     async run() {
+
         await Engine3D.init({ beforeRender: () => this.update() });
 
         let view = new View3D();
@@ -28,6 +30,7 @@ class Smaple_VertexAnimation {
     }
 
     private createScene() {
+        GUIHelp.init();
         // add light
         let lightObj3D = new Object3D();
         let directLight = lightObj3D.addComponent(DirectLight);
@@ -48,19 +51,25 @@ class Smaple_VertexAnimation {
         let renderer = floor.addComponent(MeshRenderer);
         renderer.geometry = this.floorGeometry;
         renderer.material = new LitMaterial();
+        renderer.castShadow = true;
+        renderer.receiveShadow = true;
         this.scene.addChild(floor);
     }
 
     private update() {
-        // update its vertex data over time
         if (this.floorGeometry) {
-            let attribute = this.floorGeometry.getAttribute(VertexAttributeName.position);
+            let posAttrData = this.floorGeometry.getAttribute(VertexAttributeName.position);
+
+            // update its vertex data over time
             let timeOffset = Time.time;
-            for (let i = 0, count = attribute.data.length / 3; i < count; i++) {
-                attribute.data[i * 3 + 1] = Math.sin(timeOffset * 0.01 + i * 0.25);
+            for (let i = 0, count = posAttrData.data.length / 3; i < count; i++) {
+                posAttrData.data[i * 3 + 1] = Math.sin(timeOffset * 0.01 + i * 0.25);
             }
-            // it need to be upload
-            this.floorGeometry.vertexBuffer.upload(VertexAttributeName.position, attribute);
+            // position attr need to be upload
+            this.floorGeometry.vertexBuffer.upload(VertexAttributeName.position, posAttrData);
+
+            //update normals
+            this.floorGeometry.computeNormals();
         }
     }
 }
