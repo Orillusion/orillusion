6c2b20fbf5666cfe31d7cd65fce0ceb4c0acbab7
sirxu <397939361@qq.com>
2023-05-14 23:07:34 +0800

    fix(pointShadow): point shadow not remove
    fix point shadow not remove

--- samples/base/Sample_AddRemove.ts
+++ samples/base/Sample_AddRemove.ts
@@ -51,7 +51,6 @@ class Sample_AddRemove {
             if (obj) {
                 list.splice(index, 1)
                 this.view.scene.removeChild(obj)
-                obj.destroy();
             }
         });
 
