6cc923463a3a4179fe6014e619e2e88d455416b9
hellmor <dymmky@126.com>
2023-05-25 18:38:45 +0800

    fix(GUI): revert a sample.
    revert a sample.

--- samples/gui/Sample_UISpriteSheet.ts
+++ samples/gui/Sample_UISpriteSheet.ts
@@ -1,37 +1,17 @@
-﻿import { Engine3D, GUIConfig, Object3D, Object3DUtil, Vector2, ViewPanel, WorldPanel } from "@orillusion/core";
+﻿import { Engine3D, Object3D, Object3DUtil, WorldPanel } from "@orillusion/core";
 import { UIImage } from "../../src/components/gui/uiComponents/UIImage";
 import { GUIHelp } from "@orillusion/debug/GUIHelp";
 import { createExampleScene } from "@samples/utils/ExampleScene";
 
-class SpriteSheet {
-    img: UIImage;
-    private lastIndex: number = -1;
-    private frameStart = 65;//65~77
-    private frame: number = 100 * Math.random();
-    private frameCount = 13;
-    constructor(img: UIImage) {
-        this.img = img;
-    }
-
-    updateFrame(): void {
-        this.frame++;
-        let newIndex = Math.floor(this.frame * 0.05) % this.frameCount;
-        if (newIndex != this.lastIndex) {
-            this.lastIndex = newIndex;
-            let frameKey = (this.lastIndex + this.frameStart).toString().padStart(5, '0');
-            this.img.sprite = Engine3D.res.getGUISprite(frameKey);
-        }
-    }
-}
-
 export class Sample_UISpriteSheet {
+
+    img: UIImage;
+
     async run() {
         Engine3D.setting.shadow.autoUpdate = true;
         Engine3D.setting.shadow.shadowBias = 0.002;
 
-        GUIConfig.quadMaxCountForView = 10000;
         GUIHelp.init();
-        this.spriteSheets = [];
 
         await Engine3D.init({ renderLoop: () => { this.renderUpdate(); } });
         let exampleScene = createExampleScene();
@@ -45,11 +25,17 @@ export class Sample_UISpriteSheet {
         let panelRoot: Object3D = new Object3D();
         panelRoot.scaleX = panelRoot.scaleY = panelRoot.scaleZ = 0.4;
         //create panel
-        let panel = panelRoot.addComponent(ViewPanel, { billboard: true });
+        let panel = panelRoot.addComponent(WorldPanel, { billboard: true });
         canvas.addChild(panel.object3D);
 
-        //create sprite sheet list
-        this.createSpriteSheets(panelRoot);
+        //
+        let quad = new Object3D();
+        panelRoot.addChild(quad);
+
+        this.img = quad.addComponent(UIImage);
+        this.img.sprite = Engine3D.res.getGUISprite('00065');
+        this.img.uiTransform.resize(256, 256);
+
         // create floor
         let floor = Object3DUtil.GetSingleCube(50, 5, 50, 0.5, 0.5, 0.5);
         exampleScene.scene.addChild(floor);
@@ -59,30 +45,23 @@ export class Sample_UISpriteSheet {
         exampleScene.scene.addChild(box);
     }
 
-    spriteSheets: SpriteSheet[];
-
-    private createSpriteSheets(root: Object3D) {
-        let width = Engine3D.width;
-        let height = Engine3D.height;
-        for (let i = 0; i < 8000; i++) {
-            let quad = new Object3D();
-            root.addChild(quad);
-
-            let img = quad.addComponent(UIImage);
-            img.sprite = Engine3D.res.getGUISprite('00065');
-            img.uiTransform.resize(128, 128);
-            img.uiTransform.x = (Math.random() - 0.5) * width * 0.8;
-            img.uiTransform.y = (Math.random() - 0.5) * height * 0.8;
-            let sheet: SpriteSheet = new SpriteSheet(img);
-            this.spriteSheets.push(sheet);
-        }
-
-    }
-
+    private frame: number = 10;
     renderUpdate() {
-        for (const item of this.spriteSheets) {
-            item.updateFrame();
+        if (this.img) {
+            this.frame++;
+            this.loopTextureSheet();
         }
     }
 
+    private lastIndex: number = -1;
+    private frameStart = 65;//65~77
+    private frameCount = 13;
+    loopTextureSheet(): void {
+        let newIndex = Math.floor(this.frame * 0.05) % this.frameCount;
+        if (newIndex != this.lastIndex) {
+            this.lastIndex = newIndex;
+            let frameKey = (this.lastIndex + this.frameStart).toString().padStart(5, '0');
+            this.img.sprite = Engine3D.res.getGUISprite(frameKey);
+        }
+    }
 }
