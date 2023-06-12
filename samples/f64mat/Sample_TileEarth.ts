b373ee6abf33cf2e648128f7d745d61b4c57352b
zhubingxin1119 <9415188+zhubingxin1119@user.noreply.gitee.com>
2023-06-12 09:00:57 +0800

    新建f64mat实例

---
+++ samples/f64mat/Sample_TileEarth.ts
@@ -0,0 +1,19 @@
+import { Object3D, Scene3D, AnimationCurve, Engine3D, AtmosphericComponent, CameraUtil, HoverCameraController, View3D, DirectLight, KelvinUtil, Keyframe, Object3DUtil, Time } from "@orillusion/core";
+
+class Sample_TileEarth {
+    scene: Scene3D;
+
+    async run() {
+
+    }
+
+    async initScene() {
+
+    }
+
+    renderUpdate() {
+
+    }
+}
+
+new Sample_TileEarth().run();
