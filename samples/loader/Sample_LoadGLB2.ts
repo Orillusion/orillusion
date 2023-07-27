24d0096fa9dd1d3993968538d0f7cef4c384097a
sirxu <397939361@qq.com>
2023-07-27 11:28:29 +0800

    chore: chore sample light
    chore sample light

--- samples/loader/Sample_LoadGLB2.ts
+++ samples/loader/Sample_LoadGLB2.ts
@@ -1,5 +1,8 @@
 import { Engine3D, LitMaterial, MeshRenderer, Object3D, PlaneGeometry, Scene3D } from "@orillusion/core";
+import { GUIHelp } from "@orillusion/debug/GUIHelp";
+import { GUI } from "@orillusion/debug/dat.gui.module";
 import { createExampleScene } from "@samples/utils/ExampleScene";
+import { GUIUtil } from "@samples/utils/GUIUtil";
 
 // Sample to load glb file
 export class Sample_LoadGLB2 {
@@ -10,12 +13,17 @@ export class Sample_LoadGLB2 {
         Engine3D.setting.shadow.autoUpdate = true;
         Engine3D.setting.shadow.shadowBias = 0.0001;
         let exampleScene = createExampleScene();
+        exampleScene.atmosphericSky.displaySun = false;
+        exampleScene.atmosphericSky.sunRadiance = 1;
         this.scene = exampleScene.scene;
 
         exampleScene.hoverCtrl.setCamera(-45, -45, 10);
         exampleScene.light.intensity = 10;
         Engine3D.startRenderView(exampleScene.view);
         await this.initScene();
+
+        GUIHelp.init();
+        GUIUtil.renderAtomosphericSky(exampleScene.atmosphericSky);
     }
 
     async initScene() {
