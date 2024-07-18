5ab46a9a97f003d6a3448d86f657fe3f56e0c9b6
ShuangLiu <lslzl3000@gmail.com>
2024-07-18 22:16:56 +0800

    Update BitmapTextureCube.ts

--- src/textures/BitmapTextureCube.ts
+++ src/textures/BitmapTextureCube.ts
@@ -167,7 +167,7 @@ export class BitmapTextureCube extends TextureCube {
     /**
      * load texture data from array of web url.
      * make sure there are six images in a group,
-     * and the order is: px, nx, py, ny, pz, nz
+     * and the order is: [+X, -X, +Y, -Y, +Z, -Z]
      * @param urls array of image url
      */
     public async load(urls: string[]) {
